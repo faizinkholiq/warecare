f7445eef6bc7c686f3fd292214c5ce55663d150f
faizin <nasrunfaizinkholiq@gmail.com>
2025-07-04 19:43:12 +0700

    update migrations script

--- db/migrations/02_currency.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS warecare.currency (
-    id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    code varchar(10) NOT NULL,
-    name varchar(50) NOT NULL
-);
-
-INSERT IGNORE INTO warecare.currency (id, code, name) VALUES 
-    (1, 'USD', 'US Dollar'),
-    (2, 'IDR', 'Indonesian Rupiah');
--- db/migrations/07_update_category.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `warecare`.`transaction_category` (`id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(100) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB; 
-ALTER TABLE `transaction` CHANGE `category` `category` SMALLINT NOT NULL; 
-
-INSERT INTO `transaction_category` (`id`, `name`) VALUES ('1', 'Category 1'), ('2', 'Category 2');
-INSERT INTO `transaction_status` (`id`, `name`) VALUES ('2', 'Unpaid');
-
-ALTER TABLE `transaction` CHANGE `currency` `currency` VARCHAR(10) NOT NULL; 
